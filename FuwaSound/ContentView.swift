ffffa8b582b9ce4c216e29c4f9471635a3795708
touyou <fujiyou1121@gmail.com>
2020-12-28 10:09:14 +0900

    ぴえん

--- FuwaSound/ContentView.swift
+++ FuwaSound/ContentView.swift
@@ -6,11 +6,19 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 249 / 250.0, green: 248.0 / 250.0, blue: 0.0, opacity: 1.0).edgesIgnoringSafeArea(.all)
+            Image("fuwa")
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            Button("あいう", action: {
+                let player = try! AVAudioPlayer(data: NSDataAsset(name: "fuwaSound")!.data)
+                print(player.play())
+            })
+        }
     }
 }
 
