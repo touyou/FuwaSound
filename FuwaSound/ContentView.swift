bdd9010727b92b8e6d7d12a92af9b332804ac9e3
touyou <fujiyou1121@gmail.com>
2020-12-28 10:28:37 +0900

    debug

--- FuwaSound/ContentView.swift
+++ FuwaSound/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
+    let player = try! AVAudioPlayer(data: NSDataAsset(name: "fuwaSound")!.data)
+
     var body: some View {
         ZStack {
             Color(red: 249 / 250.0, green: 248.0 / 250.0, blue: 0.0, opacity: 1.0).edgesIgnoringSafeArea(.all)
             Image("fuwa")
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             Button(action: {
-                let player = try! AVAudioPlayer(data: NSDataAsset(name: "fuwaSound")!.data)
                 player.play()
             }) {
                 Color(.sRGB, white: 0.0, opacity: 0.0)
