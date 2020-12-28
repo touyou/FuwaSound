f37c592fd4c59fa3110de32563d7c95a1814889c
touyou <fujiyou1121@gmail.com>
2020-12-28 09:47:22 +0900

    Initial Commit

---
+++ FuwaSound/FuwaSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuwaSoundApp.swift
+//  FuwaSound
+//
+//  Created by 藤井陽介 on 2020/12/28.
+//
+
+import SwiftUI
+
+@main
+struct FuwaSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
